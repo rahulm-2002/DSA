93a0ae4e1199297ca7a7545e34b62d36ed74442f
Rahul.M <rahulaurthur@gmail.com>
2026-05-11 11:48:19 +0530

    Implement binary addition in AddBinary class

---
+++ AddBin.cs
@@ -0,0 +1,29 @@
+public class Solution {
+    public string AddBinary(string a, string b)
+    {
+        int i = a.Length - 1;
+        int j = b.Length - 1;
+        int carry = 0;
+
+        System.Text.StringBuilder result = new System.Text.StringBuilder();
+
+        while (i >= 0 || j >= 0 || carry > 0) {
+            int sum = carry;
+
+            if (i >= 0) {
+                sum += a[i] - '0';
+                i--;
+            }
+
+            if (j >= 0) {
+                sum += b[j] - '0';
+                j--;
+            }
+
+            result.Insert(0, (sum % 2).ToString());
+            carry = sum / 2;
+        }
+
+        return result.ToString();
+    }
+}
